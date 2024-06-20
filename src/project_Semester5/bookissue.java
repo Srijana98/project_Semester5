ba3f9b1a152e1019a0fc3bf90dc1488582cddbf8
Srijana98 <srijana.neupane897@gmail.com>
2024-06-20 15:26:10 +0545

    Assignment 2

---
+++ src/project_Semester5/bookissue.java
@@ -0,0 +1,50 @@
+package project_Semester5;
+
+public class bookissue {
+	
+	    private int studentId;
+	    private int bookId;
+	    private String issueDate;
+	    private String dueDate;
+
+	    public bookissue(int studentId, int bookId, String issueDate, String dueDate) {
+	        this.studentId = studentId;
+	        this.bookId = bookId;
+	        this.issueDate = issueDate;
+	        this.dueDate = dueDate;
+	    }
+
+	    public int getStudentId() {
+	        return studentId;
+	    }
+
+	    public void setStudentId(int studentId) {
+	        this.studentId = studentId;
+	    }
+
+	    public int getBookId() {
+	        return bookId;
+	    }
+
+	    public void setBookId(int bookId) {
+	        this.bookId = bookId;
+	    }
+
+	    public String getIssueDate() {
+	        return issueDate;
+	    }
+
+	    public void setIssueDate(String issueDate) {
+	        this.issueDate = issueDate;
+	    }
+
+	    public String getDueDate() {
+	        return dueDate;
+	    }
+
+	    public void setDueDate(String dueDate) {
+	        this.dueDate = dueDate;
+	    }
+	}
+
+
